f3cdb3d72f48f68594aa2d6c7cf709b83e0aecc3
Rodrigo <rodrigodiasf@gmail.com>
2020-06-16 17:40:55 +0100

    Adicionado repository

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter  from './appointments.routes';
 
 const routes = Router();
+routes.use('/appointments', appointmentsRouter);
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello World' }),
-);
 
 export default routes;
